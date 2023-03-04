83b63068c52191b381e85e7524b346df956b30f8
Javad Naghiloo <contact@naghiloo.com>
2023-03-04 18:34:39 +0330

    feat(alb): add alb test project

--- environments/develop/main.tf
+++ environments/develop/main.tf
@@ -2,16 +2,73 @@ module "vpc" {
   source = "../../modules/vpc"
 
   name = var.vpc_name
+  cidr_block = var.vpc_cidr_block
 }
 
-module "subne_public" {
+module "igw" {
+  source = "../../modules/internet-gateway"
+
+  name   = var.igw_name
+  vpc_id = module.vpc.id
+
+  depends_on = [
+    module.vpc
+  ]
+}
+
+module "subnet_public1" {
   source = "../../modules/subnet"
 
-  name = var.subnet_public_name
+  name = "public1"
   cidr_block = var.subnet_public_cidr_block
   availability_zone = var.subnet_public_az
 
   vpc_id = module.vpc.id
+
+  depends_on = [
+    module.vpc
+  ]
+}
+
+module "subnet_public2" {
+  source = "../../modules/subnet"
+
+  name = "public2"
+  cidr_block = "172.16.128.0/24"
+  availability_zone = "eu-central-1b"
+
+  vpc_id = module.vpc.id
+
+  depends_on = [
+    module.vpc
+  ]
+}
+
+module "security_group" {
+  source = "../../modules/security-group"
+
+  name = var.sg_name
+  vpc_id = module.vpc.id
+
+  depends_on = [
+    module.vpc
+  ]
+}
+
+module "sg_rule_http" {
+  source = "../../modules/security-group-rule"
+
+  type = var.sgr_type
+  from_port = var.sgr_from_port
+  to_port = var.sgr_to_port
+  protocol = var.sgr_protocol
+  cidr_blocks = var.sgr_cidr_block
+
+  sg_id = module.security_group.id
+
+  depends_on = [
+    module.security_group
+  ]
 }
 
 module "key_pair" {
@@ -19,13 +76,47 @@ module "key_pair" {
   name = var.key_name
 }
 
-module "ec2_elf" {
-  source = "../../modules/ec2"
+module "target_group" {
+  source = "../../modules/target-group"
 
-  name = var.ec2_elf_name
-  ami = var.ec2_ami
-  instance_type = var.instance_type
-  security_groups = var.security_groups
-  key_name = var.key_name
-  subnet_id = module.subne_public.id
+  name = var.tg_name
+  protocol = var.tg_protocol
+  port = var.tg_port
+
+  vpc_id = module.vpc.id
+
+  depends_on = [
+    module.vpc
+  ]
+}
+
+module "asg" {
+  source = "../../modules/autoscaling-group"
+
+  lc_name = var.lc_name
+  ami = var.aws_ami
+  instance_type = var.aws_instance_type
+  key_name = module.key_pair.name
+
+  security_groups = [ module.security_group.id ]
+
+  asg_name = var.asg_name
+  target_group_arns = [ module.target_group.arn ]
+  health_check_type = var.asg_health_check_type
+  termination_policies = [ "OldestInstance" ]
+  
+  vpc_id = module.vpc.id
+  aws_region = var.aws_region
+}
+
+module "alb" {
+  source = "../../modules/load-balancer"
+
+  name = var.lb_name
+  subnets = [ module.subnet_public1.id, module.subnet_public2.id ]
+  port = var.lb_port
+  protocol = var.lb_protocol
+  action_type = var.lb_action_type
+  security_groups = [ module.security_group.id ]
+  target_group_arn = module.target_group.arn
 }
--- environments/develop/variables.tf
+++ environments/develop/variables.tf
@@ -1,5 +1,26 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "aws_ami" {
+  type = string
+}
+
+variable "aws_instance_type" {
+  type = string
+}
+
 # vpc
 variable "vpc_name" {
+  type = string
+}
+
+variable "vpc_cidr_block" {
+  type = string
+}
+
+#igw
+variable "igw_name" {
   type = string
 }
 
@@ -16,20 +37,76 @@ variable "subnet_public_az" {
   type = string
 }
 
+# security_group
+variable "sg_name" {
+  type = string
+}
+
+# security_group_rule
+variable "sgr_type" {
+  type = string
+}
+
+variable "sgr_from_port" {
+  type = number
+}
+
+variable "sgr_to_port" {
+  type = number
+}
+
+variable "sgr_protocol" {
+  type = string  
+}
+
+variable "sgr_cidr_block" {
+  type = list(string)
+}
+
 # key_pair
 variable "key_name" {
   type = string
 }
 
-# ec2
-variable "ec2_elf_name" {
+# asg
+variable "lc_name" {
   type = string
 }
 
-variable "ec2_ami" {
+variable "asg_name" {
   type = string
 }
 
-variable "instance_type" {
+variable "asg_health_check_type" {
   type = string
+}
+
+# lb
+variable "lb_name" {
+  type = string
+}
+
+variable "lb_action_type" {
+  type = string
+}
+
+variable "lb_protocol" {
+  type = string
+}
+
+variable "lb_port" {
+  type = number
+}
+
+# target_group
+variable "tg_name" {
+  type = string
+}
+
+variable "tg_protocol" {
+  type =string
+}
+
+variable "tg_port" {
+  type = number
 }
