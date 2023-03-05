82c14ae4b2fe139599d433d3df6b2d04dfc5ed05
aryanrhm <ary.rahimi@gmail.com>
2023-03-05 14:50:44 +0000

    add(s3-backend): add bucket backend configuration

--- environments/develop/provider.tf
+++ environments/develop/provider.tf
@@ -3,19 +3,12 @@ terraform {
   backend "s3" {
     bucket = "aws-terraform-tfstate-v1"
     key    = "aws-terraform/terraform.tfstate"
-    region = var.aws_region
+    region = "eu-central-1"
   }
-  required_providers {
+    required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
   }
-}
-
-provider "aws" {
-  region = var.aws_region
-  shared_config_files      = ["~/.aws/config"]
-  shared_credentials_files = ["~/.aws/credentials"]
-
 }
--- variables.tf
+++ variables.tf
@@ -2,8 +2,3 @@ variable "aws_region" {
   type = string
   default = "eu-central-1"
 }
-
-variable "instance_type" {
-  type = string
-  default = "t2.micro"
-}
